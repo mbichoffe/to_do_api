8e2a68ddb8847f8434eaac14852a0985e1632a9c
mbichoffe <bichoffe.marina@gmail.com>
2018-03-06 13:16:19 -0800

    adds 404 error handler to return a json instead of html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from model import connect_to_db, Tasks, db
 
 app = Flask(__name__)
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
 def get_tasks():
     tasks = [t.to_dict() for t in Tasks.query.all()]
     return jsonify(tasks=tasks)
@@ -13,6 +13,12 @@ def get_tasks():
 def get_task(task_id):
     t = Tasks.query.get_or_404(task_id)
     return jsonify(t.to_dict())
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
     connect_to_db(app)
