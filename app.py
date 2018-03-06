6a7f4c62606a70f3fce26ef5f67ba82b8a439fd6
mbichoffe <bichoffe.marina@gmail.com>
2018-03-06 13:42:25 -0800

    adds endpoint to create a task on the database

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from model import connect_to_db, Tasks, db
 
 app = Flask(__name__)
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET', 'POST'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
+
     tasks = [t.to_dict() for t in Tasks.query.all()]
     return jsonify(tasks=tasks)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def add_tasks():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    else:
+        new_task = Tasks(task_title=request.json['title'],
+                         task_description=request.json.get('description', ''))
+        db.session.add(new_task)
+        db.session.commit()
+
+    # Return HTTP status code 201 (the code for created), with body being new ID
+    return jsonify(new_task.task_id), 201
+
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
