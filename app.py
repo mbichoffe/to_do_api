31d8611717118b429a65385ade224dca1f08213f
mbichoffe <bichoffe.marina@gmail.com>
2018-03-06 13:11:18 -0800

    adds endpoint to query task by id

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from model import connect_to_db, Tasks, db
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def get_tasks():
     tasks = [t.to_dict() for t in Tasks.query.all()]
     return jsonify(tasks=tasks)
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    t = Tasks.query.get_or_404(task_id)
+    return jsonify(t.to_dict())
 
 if __name__ == '__main__':
     connect_to_db(app)
